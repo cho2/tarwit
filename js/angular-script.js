d6ec94bde8a60305836bee258f412d16b84847b3
Kukuh Syafaat <kukuh@syafaat.id>
2022-10-24 13:37:29 +0700

    Update domain

--- js/angular-script.js
+++ js/angular-script.js
@@ -7,7 +7,7 @@ crudApp.controller("DbController",['$scope','$http', function($scope,$http){
 	function getInfo(){
 	// Sending request to EmpDetails.php files
 			// CHANGE with your domain
-			$http.post('https://artemtech.me/tarwit/databaseFiles/empDetails.php').success(function(data){
+			$http.post('https://visitor.opensuse.id/databaseFiles/empDetails.php').success(function(data){
 		// Stored the returned data into scope
 		$scope.details = data;
 		});
@@ -53,7 +53,7 @@ crudApp.controller("DbController",['$scope','$http', function($scope,$http){
 
 	$scope.insertInfo = function(info){
 		// CHANGE with your domain
-		$http.post('https://artemtech.me/tarwit/databaseFiles/insertDetails.php',{"name":info.name,"country":info.country,
+		$http.post('https://visitor.opensuse.id/databaseFiles/insertDetails.php',{"name":info.name,"country":info.country,
 		"comment":info.comment,"photo":info.photo,"email":"","twitterid":info.twitterid}).success(function(data){
 			if (data == true) {
 				getInfo();
